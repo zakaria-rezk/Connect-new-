31792d0d69e01adac57aec95affa435f5bfbe318
elia rez2 <zakariarezk0@gmail.com>
2024-04-13 04:24:14 +0200

    adding navagation guards

--- src/main.js
+++ src/main.js
@@ -7,20 +7,16 @@ import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
 
-import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
-
-
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faLocation } from '@fortawesome/free-solid-svg-icons'
-
 /* add icons to the library */
-library.add(faLocation,faUser)
+library.add(faLocation,faUser);
+
 const app=createApp(App)
 app.use(createPinia())
 app.component('font-awesome-icon', FontAwesomeIcon)
--- src/router/index.js
+++ src/router/index.js
@@ -1,96 +1,104 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import { authStore } from "@/sotre.js/authentication/authSotre";
 
-
- const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/login',
-      name:'Login',
-      component: ()=> import('../components/register/Login.vue'),
-      meta:{requierAuth:true},
+      path: "/login",
+      name: "Login",
+      component: () => import("../components/register/Login.vue"),
+      meta: { requierAuth: false },
     },
     {
-      path: '/register',
-      name:'register',
-      component: ()=> import('../components/register/Register.vue'),
-      meta:{requierAuth:false},
+      path: "/register",
+      name: "register",
+      component: () => import("../components/register/Register.vue"),
+      meta: { requierAuth: false },
     },
     {
-      path: '/Connect.Com',
-      name:'connect',
-      component: ()=> import('../components/pages/HomePage.vue'),
-      
+      path: "/Connect.Com",
+      name: "connect",
+      component: () => import("../components/pages/HomePage.vue"),
     },
     {
-      path: '/customer/:id',
-      name:'customerWithId' ,
-      component: ()=> import ('../components/profilePager/customer/customerProfile.vue'),
-      meta:{requierAuth:true},
-      children:[
+      path: "/customer/:id",
+      name: "customerWithId",
+      component: () =>
+        import("../components/profilePager/customer/customerProfile.vue"),
+         meta: { requierAuth: true },
+      children: [
         {
-          path: 'about',
-          name: 'aboutCustomer',
-          component: () => import('../components/profilePager/customer/aboutCustomer.vue')
+          path: "about",
+          name: "aboutCustomer",
+          component: () =>
+            import("../components/profilePager/customer/aboutCustomer.vue"),
+        },
+        {
+          path: "projects",
+          name: "customerProjects",
+          component: () =>
+            import("../components/profilePager/customer/customerProject.vue"),
+        },
+        {
+          path: "protfolio",
+          name: "customerProtfolio",
+          component: () =>
+            import("../components/profilePager/customer/customerProtfolio.vue"),
+        },
+      ],
+    },
+
+    {
+      path: "/HotelAdmin",
+      name: "hotel-admin",
+      component: () =>
+        import("../components/profilePager/hotel/hotelAdminProfile.vue"),
     },
     {
-      path: 'projects',
-      name: 'customerProjects',
-      component: () => import('../components/profilePager/customer/customerProject.vue')
-},
-{
-  path: 'protfolio',
-  name: 'customerProtfolio',
-  component: () => import('../components/profilePager/customer/customerProtfolio.vue')
-},
-      ]
-    },
-  
-   
-    {
-      path: '/HotelAdmin',
-      name:'hotel-admin' ,
-      component: ()=> import ('../components/profilePager/hotel/hotelAdminProfile.vue')
+      path: "/ReservationCategories",
+      name: "reservation-categories",
+      component: () => import("../components/pages/ReservationCategories.vue"),
     },
     {
-      path: '/ReservationCategories',
-      name:'reservation-categories' ,
-      component: ()=> import ('../components/pages/ReservationCategories.vue')
+      path: "/followReservation",
+      name: "follow-reservation",
+      component: () =>
+        import("../components/profilePager/hotel/followReservation.vue"),
     },
     {
-      path: '/followReservation',
-      name:'follow-reservation' ,
-      component: ()=> import ('../components/profilePager/hotel/followReservation.vue')
+      path: "/hotelReservations",
+      name: "hotel-reservations",
+      component: () => import("../components/pages/hotelReservation.vue"),
     },
     {
-      path: '/hotelReservations',
-      name:'hotel-reservations' ,
-      component: ()=> import ('../components/pages/hotelReservation.vue')
+      path: "/addFreelanceBuisness",
+      name: "add-freelance-buisness",
+      component: () =>
+        import(
+          "../components/profilePager/freelancer/addFreelanceBuisness.vue"
+        ),
     },
     {
-      path: '/addFreelanceBuisness',
-      name:'add-freelance-buisness' ,
-      component: ()=> import ('../components/profilePager/freelancer/addFreelanceBuisness.vue')
+      path: "/addReservationBuisness",
+      name: "add-reservation-buisness",
+      component: () =>
+        import("../components/profilePager/hotel/addReservationBuisness.vue"),
     },
+
     {
-      path: '/addReservationBuisness',
-      name:'add-reservation-buisness' ,
-      component: ()=> import ('../components/profilePager/hotel/addReservationBuisness.vue')
+      path: "/",
+      name: "Connect",
+      redirect: (to) => ({ path: "/Connect.Com" }),
     },
-    
-    {
-      path: '/',
-      name:'Connect',
-      redirect: to => ({path: '/Connect.Com'}),
-    },
-  
+
     // ...other routes
   ],
 });
-router.beforeEach(async (to, from)=>{
-  if(to.meta.requierAuth){
-    return '/ReservationCategories'
-  }
-
-})
+router.beforeEach(async (to,from,next) => {
+  const store = authStore();
+  if(to.meta.requierAuth && !store.isAuth) next({name:'Login'})
+  else next()
+  
+});
 export default router;
